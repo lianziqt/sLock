bb4a9468ff7a99076c2c8a520d43c29a2e5e1bda
lzqt <myh0031@163.com>
2018-02-22 10:36:03 +0800

    站在添加权限角色的十字路口

---
+++ app/main/forms.py
@@ -0,0 +1,35 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, BooleanField, SelectField,\
+    SubmitField,DateField
+from wtforms.validators import DataRequired, Length, Email, Regexp
+from wtforms import ValidationError
+from flask_pagedown.fields import PageDownField
+from ..models import Message
+
+class MseeageForm(FlaskForm):
+    detals = PageDownField("What's on your mind?", validators=[DataRequired()])
+    typee = SelectField("信息类别", validators=[DataRequired()], choices=[('1','水'),('2','电'),('3','气'),('4','其它类'),('5','非维修申请')])
+    submit = SubmitField('Submit')
+
+class RecordSearchForm(FLaskForm):
+    dor_name = StringField("宿舍名")
+    student = StringField("学生学号")
+    time = DateField('开锁日期')
+    submit = SubmitField("提交")
+
+class DormitorySearchForm(FlaskForm):
+    student_num = SelectField("宿舍人数",choices=[('1','1'),('2','2'),('3','3'),('4','4')])
+    lock_online = BooleanField("门锁状态")
+    submit = SubmitField("提交")
+
+class StudentSearchForm(FlaskForm):
+    id = StringField("学号")
+    name = StringField("姓名")
+    dor_name = StringField("宿舍")
+    year = StringField("入学年份")
+    submit = SubmitField("提交")
+    
+class MessageSearchForm(FlaskForm):
+    dor_name = StringField("宿舍")
+    typee = SelectField("信息类别", validators=[DataRequired()], choices=[('1','水'),('2','电'),('3','气'),('4','其它类'),('5','非维修申请')])
+    submit = SubmitField(提交)
--- app/models.py
+++ app/models.py
@@ -14,11 +14,65 @@ class Permission:
     MANAGER = 4
     ADMIN = 8
 
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    default = db.Column(db.Boolean, default=False, index=True)
+    permissions = db.Column(db.Integer)
+    student = db.relationship('Student', backref='role', lazy='dynamic')
+    dormitory = db.relationship('Dormitory', backref='role', lazy='dynamic')
+    manager = db.relationship('Manager', backref='role', lazy='dynamic')
+
+    def __init__(self, **kwargs):
+        super(Role, self).__init__(**kwargs)
+        if self.permissions is None:
+            self.permissions = 0
+
+    @staticmethod
+    def insert_roles():
+        roles = {
+            'Student': [Permission.COMMOM],
+            'Dromitory': [Permission.COMMOM, Permission.DORLEADER],
+            'Administrator': [Permission.COMMOM, Permission.DORLEADER,
+                              Permission.ADMIN],
+        }
+        default_role = 'Student'
+        for r in roles:
+            role = Role.query.filter_by(name=r).first()
+            if role is None:
+                role = Role(name=r)
+            role.reset_permissions()
+            for perm in roles[r]:
+                role.add_permission(perm)
+            role.default = (role.name == default_role)
+            db.session.add(role)
+        db.session.commit()
+
+    def add_permission(self, perm):
+        if not self.has_permission(perm):
+            self.permissions += perm
+
+    def remove_permission(self, perm):
+        if self.has_permission(perm):
+            self.permissions -= perm
+
+    def reset_permissions(self):
+        self.permissions = 0
+
+    def has_permission(self, perm):
+        return self.permissions & perm == perm
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
 class Dormitory(db.model):
     __tablename__='dormitory'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(10), unique=True)
     password_hash = db.Column(db.String(128))
+    lock_online = db.Column(db.Boolean)
+    student_num = db.Column(db.Integer)
     building_id = db.Column(db.Integer, db.ForeignKey('managers.id'))
     messages = db.relationship('Message', backref='author', lazy='dynamic')
     students = db.relationship('Student', backref='dor', lazy='dynamic')
@@ -50,6 +104,12 @@ class Dormitory(db.model):
         dormitory.password = new_password
         db.session.add(dormitory)
         return True
+
+    def can(self, perm):
+        return self.role is not None and self.role.has_permission(perm)
+
+    def is_administrator(self):
+        return self.can(Permission.ADMIN)
 
 
 class Manager(db.model):
@@ -90,6 +150,13 @@ class Manager(db.model):
         user.password = new_password
         db.session.add(user)
         return True
+
+    def can(self, perm):
+        return self.role is not None and self.role.has_permission(perm)
+
+    def is_administrator(self):
+        return self.can(Permission.ADMIN)
+    
         
 class Student(db.model):
     __tablename__ = 'students'
@@ -101,6 +168,12 @@ class Student(db.model):
     start_year = db.Column(db.Integer)
     def __init__(self, **kwargs):
         super(Student, self).__init__(**kwargs)
+
+    def can(self, perm):
+        return self.role is not None and self.role.has_permission(perm)
+
+    def is_administrator(self):
+        return self.can(Permission.ADMIN)
 
 
 class Record(db.model):
@@ -134,6 +207,7 @@ class Message(db.model):
     typee = db.Column(db.Integer)
     time = db.Column(db.DateTime(), index=True,  default=datetime.utcnow)
     details = db.Column(db.Text())
+    complete = db.Column(db.Boolean())
 
     def __init__(self, **kwargs):
         super(Message, self).__init__(**kwargs)
